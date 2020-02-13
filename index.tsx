78251d276e4a2f4543d07278c6b029c61587df67
八坂直人 <yasakanaoto@yasakanaotonoMacBook-Pro.local>
2020-02-13 22:54:06 +0900

    header menu list component

---
+++ index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import styled from "styled-components";
+
+import { App } from './components/App';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
